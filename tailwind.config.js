a9ace451d4a6f6d83fe25376e6c19efc5bfa8f03
isaksolheim <mail@isak.me>
2023-11-20 20:07:35 +0100

    Update index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["content/**/*.md", "layouts/**/*.html"],
+  content: ["content/**/*.md", "layouts/**/*.html", "../../content/**/*.md"],
   theme: {
     extend: {},
   },
